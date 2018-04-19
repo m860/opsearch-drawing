a021bbcd8ef1a845356e02d8dc4470bbebe06ed7
jean <jean.ma.1986@gmail.com>
2018-04-19 18:09:49 +0800

    fix

--- src/components/InteractionGraph.js
+++ src/components/InteractionGraph.js
@@ -20,10 +20,16 @@ const drawTypeEnums = {
 const actionTypeEnums = {
 	draw: "draw",
 	select: "select",
+	unselect: "unselect",
 	delete: "delete",
 	move: "move",
 	undo: "undo",
 	data: "data",
+};
+
+const selectModeEnums = {
+	single: "single",
+	multiple: "multiple"
 };
 
 class DrawAction {
@@ -33,28 +39,75 @@ class DrawAction {
 	}
 }
 
+class SelectAction {
+	constructor(id) {
+		this.params = id;
+		this.type = actionTypeEnums.select;
+	}
+}
+
+class UnSelectAction {
+	constructor(id) {
+		this.params = id;
+		this.type = actionTypeEnums.unselect;
+	}
+}
+
+/**
+ * 运筹学图形D3
+ * - [x] action:draw - 画线
+ * - [x] action:draw - 画点
+ * - [x] action:draw - 画圈
+ * - [x] action:draw - 画文本
+ * - [ ] action:draw - 画link
+ * - [ ] action:draw - 画arrowLink
+ * - [x] action:select - 选择
+ * - [x] action:unselect - 取消选择
+ * - [ ] action:delete - 删除
+ * - [ ] action:move - 移动
+ * - [ ] action:undo - 撤销
+ * - [ ] action:data - 数据
+ * */
 export default class InteractionGraph extends PureComponent {
+	/**
+	 * @property {Array} actions - 所有的操作
+	 * @property {Object} defaultAttrs - 默认的图形的样式
+	 * @property {Object} selectedAttrs - 图形被选中时候的样式
+	 * @property {single|multiple} selectMode - 选择模式,是多选还是单选
+	 * */
 	static propTypes = {
 		width: PropTypes.number,
 		height: PropTypes.number,
 		style: PropTypes.object,
+		//action
 		actions: PropTypes.arrayOf(PropTypes.shape({
-			type: PropTypes.oneOf(['draw', 'select', 'delete', 'move', 'undo', 'data']).isRequired,
+			type: PropTypes.oneOf(['draw', 'select', 'unselect', 'delete', 'move', 'undo', 'data']).isRequired,
 			params: PropTypes.oneOfType([
+				//draw
 				PropTypes.shape({
 					id: PropTypes.string.isRequired,
 					type: PropTypes.oneOf(['none', 'line', 'link', 'arrowLink', 'dot', 'circle', 'text']).isRequired,
 					attrs: PropTypes.object,
 					text: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
-				})
+				}),
+				//select
+				PropTypes.string
 			])
 		})),
+		//默认的图形的样式
 		defaultAttrs: PropTypes.shape({
 			line: PropTypes.object,
 			dot: PropTypes.object,
 			circle: PropTypes.object,
 			text: PropTypes.object
-		})
+		}),
+		//图形被选中时候的样式
+		selectedAttrs: PropTypes.shape({
+			line: PropTypes.object,
+			dot: PropTypes.object,
+		}),
+		//选择模式,是多选还是单选
+		selectMode: PropTypes.oneOf(['single', 'multiple'])
 	};
 	static defaultProps = {
 		width: 400,
@@ -64,7 +117,7 @@ export default class InteractionGraph extends PureComponent {
 		},
 		defaultAttrs: {
 			line: {
-				fill: "none",
+				fill: "transparent",
 				stroke: "black",
 				"stroke-width": "3px"
 			},
@@ -74,15 +127,31 @@ export default class InteractionGraph extends PureComponent {
 				r: "5px"
 			},
 			circle: {
-				fill: "none",
+				fill: "transparent",
 				stroke: "black",
 				r: "10px",
 				"stroke-width": "1px"
 			},
 			text: {
+				fill: "black",
 				"font-size": "20px"
 			}
-		}
+		},
+		selectedAttrs: {
+			line: {
+				stroke: "red"
+			},
+			dot: {
+				fill: "red"
+			},
+			circle: {
+				stroke: "red"
+			},
+			text: {
+				fill: "red"
+			}
+		},
+		selectMode: "single"
 	}
 
 	constructor(props) {
@@ -90,18 +159,21 @@ export default class InteractionGraph extends PureComponent {
 		this.ele = null;
 		//画图的类型
 		this.currentDrawType = drawTypeEnums.none;
-		this.draws = [];
+		//画布中已有的图形
+		this.shapes = [];
+		//已经选中的图形的id
+		this.selectedShapeId = [];
 	}
 
 	findDrawById(id) {
-		const index = this.draws.findIndex(f => f.id === id);
-		return this.draws[index];
+		const index = this.shapes.findIndex(f => f.id === id);
+		return this.shapes[index];
 	}
 
 	updateDraw(id, draw) {
-		const index = this.draws.findIndex(f => f.id === id);
-		this.draws[index] = update(this.draws[index], draw);
-		this.draw();
+		const index = this.shapes.findIndex(f => f.id === id);
+		this.shapes[index] = update(this.shapes[index], draw);
+		this.drawShapes(this.shapes);
 	}
 
 	initialSVG() {
@@ -170,19 +242,67 @@ export default class InteractionGraph extends PureComponent {
 
 	doActions(actions) {
 		//#region draw
-		const draws = actions.filter(f => f.type === "draw").map(data => {
-			return data.params;
-		});
-		this.draws = this.draws.concat(draws);
-		this.draw();
+		console.log('do actions ...')
+		const drawActions = actions.filter(f => f.type === actionTypeEnums.draw);
+		if (drawActions.length > 0) {
+			console.log(`draw : ${drawActions.map(f => f.params.type).join(',')}`)
+			this.shapes = this.shapes.concat(drawActions.map(data => data.params));
+			this.drawShapes(this.shapes);
+		}
+		//#endregion
+
+		//#region select
+		const selectActions = actions.filter(f => f.type === actionTypeEnums.select);
+		if (selectActions.length > 0) {
+			console.log(`${this.props.selectMode} select : ${selectActions.map(f => f.params).join(',')}`)
+			if (this.props.selectMode === selectModeEnums.single) {
+				// this.unSelect(this.selectedShapeId);
+				this.doActions(this.selectedShapeId.map(id => new UnSelectAction(id)))
+				this.selectedShapeId = selectActions.map(f => f.params);
+			}
+			else {
+				this.selectedShapeId = this.selectedShapeId.concat(selectActions.map(f => f.params));
+			}
+			this.select(this.selectedShapeId);
+		}
+		//#endregion
+
+		//#region unselect
+		const unSelectActions = actions.filter(f => f.type === actionTypeEnums.unselect);
+		if (unSelectActions.length > 0) {
+			console.log(`unselect : ${unSelectActions.map(f => f.params).join(',')}`);
+			const ids = unSelectActions.map(f => f.params);
+			this.unSelect(ids);
+		}
 		//#endregion
 	}
 
-	draw() {
-		this.drawLines(this.draws.filter(f => f.type === drawTypeEnums.line));
-		this.drawDots(this.draws.filter(f => f.type === drawTypeEnums.dot));
-		this.drawCircles(this.draws.filter(f => f.type === drawTypeEnums.circle));
-		this.drawTexts(this.draws.filter(f => f.type === drawTypeEnums.text));
+	select(ids) {
+		ids.forEach(id => {
+			const draw = this.findDrawById(id);
+			if (draw) {
+				if (draw.selection) {
+					const attrs = getPath(this.props.selectedAttrs, draw.type, {})
+					draw.selection.call(self => {
+						for (let key in attrs) {
+							self.attr(key, attrs[key]);
+						}
+					})
+				}
+			}
+		})
+	}
+
+	unSelect(ids) {
+		const shapes = this.shapes.filter(f => ids.indexOf(f.id) >= 0);
+		this.drawShapes(shapes);
+	}
+
+	drawShapes(shapes) {
+		this.drawLines(shapes.filter(f => f.type === drawTypeEnums.line));
+		this.drawDots(shapes.filter(f => f.type === drawTypeEnums.dot));
+		this.drawCircles(shapes.filter(f => f.type === drawTypeEnums.circle));
+		this.drawTexts(shapes.filter(f => f.type === drawTypeEnums.text));
 	}
 
 	updateAttrs(ele, drawType, attrs) {
@@ -195,10 +315,10 @@ export default class InteractionGraph extends PureComponent {
 	drawTexts(texts) {
 		texts.forEach(text => {
 			if (!text.selection) {
-				text.selection = d3.select(this.ele).append("text");
+				text.selection = this.createShape(text, 'text');
 			}
 			text.selection.call(self => {
-				this.updateAttrs(self, drawTypeEnums.circle, text.attrs);
+				this.updateAttrs(self, drawTypeEnums.text, text.attrs);
 				if (text.text) {
 					self.text(text.text);
 				}
@@ -212,7 +332,7 @@ export default class InteractionGraph extends PureComponent {
 	drawCircles(circles) {
 		circles.forEach(circle => {
 			if (!circle.selection) {
-				circle.selection = d3.select(this.ele).append("circle");
+				circle.selection = this.createShape(circle, "circle");
 			}
 			circle.selection.call(self => {
 				this.updateAttrs(self, drawTypeEnums.circle, circle.attrs);
@@ -223,7 +343,7 @@ export default class InteractionGraph extends PureComponent {
 	drawDots(dots) {
 		dots.forEach(dot => {
 			if (!dot.selection) {
-				dot.selection = d3.select(this.ele).append("circle");
+				dot.selection = this.createShape(dot, "circle");
 			}
 			dot.selection.call(self => {
 				this.updateAttrs(self, drawTypeEnums.dot, dot.attrs);
@@ -234,12 +354,22 @@ export default class InteractionGraph extends PureComponent {
 	drawLines(lines) {
 		lines.forEach(line => {
 			if (!line.selection) {
-				line.selection = d3.select(this.ele).append("line");
+				//create line
+				line.selection = this.createShape(line, "line")
 			}
 			line.selection.call(self => {
 				this.updateAttrs(self, drawTypeEnums.line, line.attrs);
 			})
 		})
+	}
+
+	createShape(shapeDefine, tag) {
+		const shape = d3.select(this.ele).append(tag);
+		//listen click event trigger selection action
+		shape.on('click', () => {
+			this.doActions([new SelectAction(shapeDefine.id)]);
+		});
+		return shape;
 	}
 
 	render() {
