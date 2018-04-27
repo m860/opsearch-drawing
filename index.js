9f95be7903db653411e8c683adf16177352c363e
jean <jean.ma.1986@gmail.com>
2018-04-27 12:17:21 +0800

    添加主文件

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by jean.h.ma on 27/04/2018.
+ */
+module.exports = {
+	D3Graph: require("./components/D3Graph").default,
+	InteractionTable: require('./components/InteractionTable').default,
+	WorkSpace: require('./components/WorkSpace').default
+};
--- src/components/InteractionLine.js
+++
@@ -1,9 +0,0 @@
-import React, {PureComponent} from "react";
-
-export default class InteractionLine extends PureComponent {
-	render() {
-		return (
-			<p>hello test component!</p>
-		);
-	}
-}
