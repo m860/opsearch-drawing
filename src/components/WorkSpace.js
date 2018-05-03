33d5a85d913da22abe6ca9b2f1da367601095731
jean <jean.ma.1986@gmail.com>
2018-05-03 10:07:07 +0800

    调整WorkSpace,如果没有actions就不展示

--- src/components/WorkSpace.js
+++ src/components/WorkSpace.js
@@ -10,10 +10,18 @@ export default class WorkSpace extends PureComponent {
 		style: PropTypes.object
 	};
 
+	get hasActions() {
+		if (this.props.actions) {
+			return true;
+		}
+		return false;
+	}
+
 	render() {
 		return (
 			<div style={this.props.style} className={classNames("work-space", this.props.className)}>
-				<div className="work-space-actions">{this.props.actions}</div>
+				{this.hasActions &&
+				<div className="work-space-actions">{this.props.actions}</div>}
 				<div className="work-space-content">{this.props.children}</div>
 			</div>
 		);
